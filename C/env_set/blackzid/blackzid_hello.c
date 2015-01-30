73e29c322fe2434d10f9a5e33bfccb8c4a0c684f
Veck Hsiao <fbukevin@gmail.com>
2015-01-30 08:56:58 +0800

    All practice1 are finished

--- C/env_set/blackzid/blackzid_hello.c
+++ C/env_set/blackzid/blackzid_hello.c
@@ -1,2 +1,8 @@
 #include<stdio.h>
-int main(){printf("hello world\n"); return 0;}
+
+int main(){
+    
+    printf("hello world\n"); 
+    printf("Check by Veck on Jan/30, 2015: OK\n");
+    return 0;
+}
--- C/env_set/soraxlight/soraxlight_hello.c
+++ C/env_set/soraxlight/soraxlight_hello.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int main(){
-	printf("Hello World! \n");
-return 0;
+    printf("Hello World! \n");
+    printf("Check by Veck on Jan/30, 2015: OK\n");
+    return 0;
 }
